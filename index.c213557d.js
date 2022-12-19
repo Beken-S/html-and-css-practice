d200a50049b6332c0d4f484fed226da7a42e6231
Beken Sarsenbaev <sbeken6@gmail.com>
2022-12-19 10:50:20 +0300

    Updates

---
+++ index.c213557d.js
@@ -0,0 +1,2 @@
+var grid=document.createElement("div");grid.className="grid",document.body.appendChild(grid),document.addEventListener("keydown",(function(e){e.ctrlKey&&"KeyG"===e.code&&grid.classList.toggle("grid_visible")}));
+//# sourceMappingURL=index.c213557d.js.map
